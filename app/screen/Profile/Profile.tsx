68190da3fcc097780a62fc4d9836dc3127d0adeb
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2025-07-18 19:25:12 +0300

    Added SignIn Form

--- app/screen/Profile/Profile.tsx
+++ app/screen/Profile/Profile.tsx
@@ -1,11 +1,13 @@
 import GradientLayout from "@/components/GradientLayout/GradientLayout";
 import { Button, View, Text, TouchableOpacity } from "react-native";
 import SignUp from "../SignUp/SignUp";
+import SignIn from "../SignIn/SignIn";
 
 export default function Profile() {
   return (
     <GradientLayout>
-      <SignUp />
+      {/* <SignUp /> */}
+      <SignIn />
     </GradientLayout>
   );
 }
